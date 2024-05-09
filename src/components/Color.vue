40f6d9b7b7dad16222c7a04ac67581a4f15deb72
Féry Mathieu (Mathius) <ferymathieuy@gmail.com>
2024-05-09 11:18:20 +0200

    feat(colors): Allow customisation of color (And show current color declaration and default declaration)

--- src/components/Color.vue
+++ src/components/Color.vue
@@ -2,7 +2,7 @@
   <v-card class="ma-2" :variant="vCardVariant">
     <v-card-title>{{ color.id }}</v-card-title>
     <v-card-text>
-      <v-color-picker class="ma-2" v-model="colorObject" v-model:mode="mode" :modes="modes" hide-mode-switch disabled>
+      <v-color-picker class="ma-2" v-model="colorObject" v-model:mode="mode" :modes="modes" hide-mode-switch>
       </v-color-picker>
       <v-select class="ma-2" v-model="mode" :items="modes" style="max-width: 300px" />
       <v-text-field
@@ -14,6 +14,15 @@
         readonly
         @click:append-inner="copyColorDeclaration"
       ></v-text-field>
+      <v-text-field
+        v-model="defaultColorDeclaration"
+        append-inner-icon="mdi-content-copy"
+        label="Default Color declaration"
+        type="text"
+        variant="outlined"
+        readonly
+        @click:append-inner="copyDefaultColorDeclaration"
+      ></v-text-field>
       Since : {{ color.since }} <br />
       <span v-if="color.colorUsed">ColorUsed : {{ color.colorUsed }}</span>
       <Usage v-if="color.usage" :usage="color.usage" />
@@ -23,7 +32,7 @@
 
 <script>
 import Usage from "./Usage.vue";
-import { localStorageColorFormatSelected } from '../core/utils.js';
+import { localStorageColorFormatSelected, colorToWakfuColor, colorDeclaration } from '../core/utils.js';
 
 let colorFormatSelected = localStorage.getItem(localStorageColorFormatSelected);
 colorFormatSelected = colorFormatSelected != null ? colorFormatSelected : "rbga";
@@ -36,40 +45,58 @@ export default {
   data: () => ({
     modes: ['rgba', 'hexa'],
     mode: colorFormatSelected,
+    colorObject: null,
     colorDeclaration: null,
+    defaultColorDeclaration: null,
   }),
 
   mounted() {
-    this.refreshColorDeclaration();
+    this.refreshColorAttributes();
   },
 
   watch: {
     mode() {
       localStorage.setItem(localStorageColorFormatSelected, this.mode);
-      this.refreshColorDeclaration();
+      this.refreshColorDeclaration(false);
     },
     color() {
-      this.refreshColorDeclaration();
+      this.refreshColorAttributes();
+    },
+    colorObject() {
+      this.refreshColorDeclaration(false);
     }
   },
 
-  computed: {
-    colorObject() {
-      return {
+  methods: {
+    refreshColorAttributes() {
+      this.refreshColorObject();
+      this.refreshColorDeclaration();
+    },
+    refreshColorObject() {
+      this.colorObject = {
         r: this.color.resolveRed(),
         g: this.color.resolveGreen(),
         b: this.color.resolveBlue(),
         a: this.color.resolveAlpha() / 100,
-      };
+      }
     },
-  },
-
-  methods: {
-    refreshColorDeclaration() {
-      this.colorDeclaration = this.color.resolveColorDeclaration(this.mode == 'hexa');
+    refreshColorDeclaration(useDefault = true) {
+      let useHexa = this.mode =='hexa';
+      if (useDefault) {
+        this.defaultColorDeclaration = this.color.resolveColorDeclaration(useHexa);
+        this.colorDeclaration = this.defaultColorDeclaration;
+      } else {
+        this.colorDeclaration = colorDeclaration(
+          this.color.id,
+          colorToWakfuColor(this.colorObject.r, this.colorObject.g, this.colorObject.b, this.colorObject.a * 100, useHexa)
+        );
+      }
     },
     async copyColorDeclaration() {
       await navigator.clipboard.writeText(this.colorDeclaration);
+    },
+    async copyDefaultColorDeclaration() {
+      await navigator.clipboard.writeText(this.colorDefaultDeclaration);
     },
   }
 
